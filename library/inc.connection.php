c83f15114ee657a254eb17a01b362ca5851c8151
Ahmad Jordi Untoro <82827167+jordi210499@users.noreply.github.com>
2021-11-02 21:52:14 +0700

    Add files via upload

---
+++ library/inc.connection.php
@@ -0,0 +1,13 @@
+<?php
+$my['host']  = "localhost";
+$my['user']  = "root";
+$my['pass']  = "";
+$my['dbs']  = "jeki";
+
+$koneksidb  = mysql_connect($my['host'], $my['user'], $my['pass']);
+if (!$koneksidb) {
+  echo "Failed Connection !";
+}
+// memilih database pda server
+mysql_select_db($my['dbs'])
+  or die("Database not Found, please contact administrator system!");
